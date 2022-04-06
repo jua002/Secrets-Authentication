803390b5853a653208524e6f5cfbc6c6abfc8d1d
Joe <joe.attah1@icloud.com>
2022-04-06 17:17:09 -0500

    Added Enviroment vars and jshint

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:8
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -25,8 +25,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "TThisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const User = mongoose.model("User", userSchema);
 
